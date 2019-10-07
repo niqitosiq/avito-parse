51da3531edcf5418a7779da6db42cdaee0061f9c
niqitosiq <macbookair@MacBook-Air-Macbook.local>
2019-10-07 20:50:44 +0400

    Few simple things for start

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"github.com/gocolly/colly"
+
+)
+
+
+func main(){
+
+	c := colly.NewCollector()
+	
+	c.OnHTML(".item-description-title-link", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnHTML(".item-description-text>p", func(e *colly.HTMLElement){
+		fmt.Println("ТЕКСТ ОБЪЯВЛЕНИЯ:", e.Text)
+	})
+
+	var link_main string
+
+	flag.StringVar(&link_main, "link", "", "Please input a link;")
+	flag.Parse()
+
+	fmt.Println("Let's look:", link_main)
+	c.Visit(link_main)
+}
